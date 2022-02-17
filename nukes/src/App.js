effe76ce64e5a6d0ac3826452a2023a0b6b9cd07
kiritocode1 <kathawalearyan9@gmail.com>
2022-02-17 21:01:39 +0530

    nice stuff

--- nukes/src/App.js
+++ nukes/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1 className="text-red-500">
+        big funny bullet in my head 
+      </h1>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
