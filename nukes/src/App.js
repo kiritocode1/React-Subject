4e9881b6cbe25546015a0be1ad65700dd5e3a72a
kiritocode1 <kathawalearyan9@gmail.com>
2022-02-18 11:41:20 +0530

    done

--- nukes/src/App.js
+++ nukes/src/App.js
@@ -1,26 +1,38 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useSpring, animated } from "react-spring";
+import Hii from "./Routes/exp";
+import { useState } from "react";
+
+
 
 function App() {
+  const [vis, setvis] = useState(false);
+    const styles = useSpring({
+      loop: true,
+      borderRadius: vis ? 16 : 90,
+      width: vis ? 70 : 180,
+      height: vis ? 70 : 180,
+      backgroundColor: vis ? "green" : "yellow",
+      color: vis ? "blue" : "brown",
+      fontSize:vis?"20px":"100px",
+    delay:200,
+  })
+
   return (
-    <div className="App">
-      <h1 className="text-red-500">
-        big funny bullet in my head 
-      </h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div className="App  h-96  flex flex-col justify-center items-center">
+      <animated.div style={{
+
+        backgroundColor: "#2b3148", 
+        color:"white",
+        ...styles,
+      }} className="flex justify-center  items-center font-mono">
+        hii
+      </animated.div>
+      <button onClick={()=>setvis(!vis)}>
+        set
+      </button>
+    <Hii name={vis}/>
     </div>
   );
 }
---
+++ nukes/src/Routes/exp.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Hii = (props) => {
+
+    return (<div>
+        {props.name}
+    </div>)
+};
+
+export default Hii;
